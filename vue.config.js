4048605616131b444a742ad5e77cd70dbce43ce9
ryanxuan930 <anakinchung930@gmail.com>
2022-06-24 15:46:21 +0800

    v1.0.2

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: '',
-    outputDir: 'dist',
+    outputDir: 'docs',
     assetsDir: 'static'
 })
